2df9aecf0d0e7cc96943bde6d5ee8fc9e72fd161
Avi Agarwal <aviagarwal1212@gmail.com>
2021-10-19 09:11:49 +0530

    Fixed IrisModel instantiation

--- project/worker.py
+++ project/worker.py
@@ -7,9 +7,11 @@ celery = Celery(__name__)
 celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379")
 celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379")
 
+model = IrisModel()
+
 @celery.task(name='create_task')
 def create_task(sepal_length, sepal_width, petal_length, petal_width):
-    model = IrisModel()
+    #model = IrisModel()
     prediction, probability= model.predict_species(sepal_length, sepal_width, petal_length, petal_width)
     return {
         'prediction': prediction,
